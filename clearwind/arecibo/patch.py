dab674812ce716c1613aa76453657eb2846c125b
sean <sean@hsc190-219.med.utah.edu>
2011-10-03 14:16:22 -0600

    do not log to arecibo in cases where site from getSite() is plone site, but error log is not contained within -- this triggers problems during any exception raised during creation of a Plone site.

--- clearwind/arecibo/patch.py
+++ clearwind/arecibo/patch.py
@@ -3,10 +3,19 @@ from zope.app.component.hooks import getSite
 from wrapper import arecibo 
 import traceback
 
+
+def is_contained(context, site):
+    if not hasattr(site, 'getPhysicalPath'):
+        return False
+    sitepath = site.getPhysicalPath()
+    l = len(sitepath)
+    return sitepath[:l] == context.getPhysicalPath()[:l]
+
+
 old_raising = SiteErrorLog.SiteErrorLog.raising
 def raising(self, *args, **kw): 
     site = getSite()
-    if site and site.meta_type == "Plone Site":
+    if site and site.meta_type == "Plone Site" and is_contained(self, site):
         err = str(getattr(args[0][0], '__name__', args[0][0]))
         tb = "\n".join(traceback.format_tb(args[0][2]))
         msg = args[0][1]
