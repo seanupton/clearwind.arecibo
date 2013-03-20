24dffef20cf4209f91ebe4288e3deec35bb42640
Sean Upton <sdupton@gmail.com>
2013-03-20 10:00:07 -0600

    do not attempt to use arecibo for logging in a site in which product is not installed

--- clearwind/arecibo/patch.py
+++ clearwind/arecibo/patch.py
@@ -16,10 +16,11 @@ old_raising = SiteErrorLog.SiteErrorLog.raising
 def raising(self, *args, **kw): 
     site = getSite()
     if site and site.meta_type == "Plone Site" and is_contained(self, site):
-        err = str(getattr(args[0][0], '__name__', args[0][0]))
-        tb = "\n".join(traceback.format_tb(args[0][2]))
-        msg = args[0][1]
-        arecibo(self, error_type=err, error_tb=tb, error_msg=msg)
+        if 'clearwind.arecibo' in site.portal_quickinstaller.keys():
+            err = str(getattr(args[0][0], '__name__', args[0][0]))
+            tb = "\n".join(traceback.format_tb(args[0][2]))
+            msg = args[0][1]
+            arecibo(self, error_type=err, error_tb=tb, error_msg=msg)
     return old_raising(self, *args, **kw)
                              
 SiteErrorLog.SiteErrorLog.raising = raising
